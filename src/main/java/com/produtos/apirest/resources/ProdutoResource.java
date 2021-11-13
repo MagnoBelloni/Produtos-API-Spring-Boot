2dfae9984f5b3b0352df53209b8bde6f60149f28
Magno Belloni <bellonidesouza6@gmail.com>
2021-11-13 12:26:46 -0300

    Deploy V1

---
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -0,0 +1,62 @@
+package com.produtos.apirest.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.produtos.apirest.repository.ProdutoRepository;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import com.produtos.apirest.models.Produto;
+
+@RestController
+@RequestMapping(value="/api")
+@Api(value="API REST PRodutos")
+@CrossOrigin(origins="*")
+public class ProdutoResource {
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
+	@GetMapping("/produto")
+	@ApiOperation(value="Retorna uma lista de produtos")
+	public List<Produto> listarProdutos() {
+		return produtoRepository.findAll();
+	}
+	
+	@GetMapping("/produto/{id}")
+	@ApiOperation(value="Retorna um produto")
+	public Produto listarProdutoDetalhe(@PathVariable(value="id")long id) {
+		return produtoRepository.findById(id);
+	}
+	
+	@PostMapping("/produto")
+	@ApiOperation(value="Salva um produto")
+	public Produto salvarProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
+	
+	@PutMapping("/produto")
+	@ApiOperation(value="Atualiza um produto")
+	public Produto atualizarProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
+	@DeleteMapping("/produto")
+	@ApiOperation(value="Deleta um produto")
+	public void deletarProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
+	}
+}
